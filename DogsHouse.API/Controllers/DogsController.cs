199d891a72e7fbf8cd5e8f195aa032abacf5c33a
coxmusyoki <coxmusyoki@outlook.com>
2024-10-30 22:07:07 +0300

    Added the Dogs Contoller

---
+++ DogsHouse.API/Controllers/DogsController.cs
@@ -0,0 +1,47 @@
+﻿using DogsHouse.Core.DTOs;
+using DogsHouse.Core.Interfaces;
+using DogsHouse.Core.Parameters;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace DogsHouse.API.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class DogsController : ControllerBase
+    {
+        private readonly IDogService _dogService;
+
+        public DogsController(IDogService dogService)
+        {
+            _dogService = dogService;
+        }
+
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("Dogshouseservice.Version1.0.1");
+        }
+
+        [HttpGet("dogs")]
+        public async Task<IActionResult> GetDogs([FromQuery] QueryParameters parameters)
+        {
+            var dogs = await _dogService.GetDogsAsync(parameters);
+            return Ok(dogs);
+        }
+
+        [HttpPost("dog")]
+        public async Task<IActionResult> CreateDog([FromBody] DogDto dogDto)
+        {
+            try
+            {
+                var result = await _dogService.CreateDogAsync(dogDto);
+                return CreatedAtAction(nameof(GetDogs), result);
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- DogsHouse.API/Program.cs
+++ DogsHouse.API/Program.cs
@@ -1,12 +1,28 @@
+using DogsHouse.Core.Interfaces;
+using DogsHouse.Core.Services;
+using DogsHouse.Infrastructure.Data;
+using DogsHouse.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DogsContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Host=localhost;Port=5433;Database=Queue;Username=postgres;Password=developer2023")));
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<IDogRepository, DogRepository>();
+builder.Services.AddScoped<IDogService, DogService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
