c39de14820fd616ed72d5c373858082ca0e4fba8
coxmusyoki <coxmusyoki@outlook.com>
2024-10-30 21:17:01 +0300

    Defined the models and the DTOs

---
+++ DogsHouse.Core/Models/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsHouse.Core.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public int TailLength { get; set; }
+        public int Weight { get; set; }
+    }
+}
