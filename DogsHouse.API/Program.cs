d63ae19daafcca966e8a3691a5c36405cafc38fc
coxmusyoki <coxmusyoki@outlook.com>
2024-10-31 19:31:43 +0300

    Modified the rate limiting middleware

--- DogsHouse.API/Program.cs
+++ DogsHouse.API/Program.cs
@@ -4,6 +4,7 @@ using DogsHouse.Core.Services;
 using DogsHouse.Infrastructure.Data;
 using DogsHouse.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,35 @@ builder.Services.AddDbContext<DogsContext>(options => options.UseNpgsql(builder.
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddScoped<IDogRepository, DogRepository>();
 builder.Services.AddScoped<IDogService, DogService>();
+
+// Program.cs
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: httpContext.User.Identity?.Name ?? httpContext.Request.Headers.Host.ToString(),
+            factory: partition => new FixedWindowRateLimiterOptions
+            {
+                AutoReplenishment = true,
+                PermitLimit = 100,
+                QueueLimit = 0,
+                Window = TimeSpan.FromMinutes(1)
+            }));
+
+    options.OnRejected = async (context, cancellationToken) =>
+    {
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            context.HttpContext.Response.Headers.RetryAfter = retryAfter.TotalSeconds.ToString();
+        }
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        await context.HttpContext.Response.WriteAsJsonAsync(new
+        {
+            error = "Too many requests. Please try again later."
+        });
+    };
+});
 
 var app = builder.Build();
 
